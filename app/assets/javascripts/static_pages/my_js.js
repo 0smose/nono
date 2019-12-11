5ad7c5625a8488ac24e51f03e62e663e12328f53
Robin Dercole <robin.dercole@gmail.com>
2019-12-11 16:03:57 +0100

    modal working again ?

--- app/assets/javascripts/static_pages/my_js.js
+++ app/assets/javascripts/static_pages/my_js.js
@@ -4,13 +4,13 @@
     var a = document.getElementsByClassName("iterator");
     for (var i = 0; i < a.length; i++) {
       a[i].href = "#menu"+(i+1);
-  }
+    }
 
-  var b = document.getElementsByClassName("image");
+    var b = document.getElementsByClassName("image");
 
-  for (var e = 0; e <b.length; e++) {
-   b[e].id = "menu" + (e + 1); 
-}
+    for (var e = 0; e <b.length; e++) {
+     b[e].id = "menu" + (e + 1); 
+   }
 
 
  // ************************************** Modal ****************************************
@@ -20,41 +20,48 @@
 
 var getImages = document.querySelectorAll(".carrd-img-top");
 for (var modal = 0; modal < getImages.length; modal++) {
-    getImages[modal].id = "myImg" + (modal + 1);
+  getImages[modal].id = "myImg" + (modal + 1);
 }
 
 
 // get the modal
 
 
-var getModal = document.querySelectorAll(".modal");
+var getModal = document.getElementsByClassName("modal");
+console.log(getModal);
 
 for (var m = 0;m<getModal.length;m++){
-    getModal[m].id = "myModal" + (m +1);
+  getModal[m].id = "myModal" + (m +1);
 }
 
 // get the modal content
 
 var getModalContent = document.getElementsByClassName('modal-content');
 for (var d = 0; d<getModalContent.length; d++) {
-    getModalContent[d].id =  "myModalContent" + (d +1);
+  getModalContent[d].id =  "myModalContent" + (d +1);
 }
 
 // Get the <span> element that closes the modal
 var mySpan = document.getElementsByClassName("close");
-console.log(mySpan);
 
 
-// for (var i = 0; i<getImages.length; i++) {
-// 	getImages[i].onclick = function() {
-// 		getModal[i].style.display = "block";
-// 		getModalContent[i].src = this.src;
-//       mySpan[i].onclick = function() {
-//       getModal[i].style.display = "none";
-//       }
-// 	} 
-		
-// }
+
+for (var x = 0; x<getImages.length; x++) {
+	getImages[x].onclick = function() {
+    for (var tg = 0; tg <getModal.length; tg++) {
+      getModal[tg].style.display = "block";
+      getModalContent[tg].src = this.src;
+      
+      mySpan[tg].onclick = function() {
+        for (var l = 0;l<getModal.length;l++){
+          getModal[l].style.display = "none";
+        }
+      }
+    }
+  }
+}
+
+
 
 
 
