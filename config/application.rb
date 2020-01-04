d821e6e29ccce58fa83d739a036058c056775ff2
Robin Dercole <robin.dercole@gmail.com>
2020-01-04 13:50:59 +0100

    fonts in scss

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Portfolio
 
 
     # for the fonts 
-    config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
   end
 end
