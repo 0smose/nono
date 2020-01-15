1b3db4e442e66fb3966820cc8543c81c95fe39a2
Robin Dercole <robin.dercole@gmail.com>
2020-01-15 12:49:19 +0100

    magnific popup works

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,28 @@
 //
 //= require rails-ujs
 //= require activestorage
+//= require magnific-popup
 //= require_tree .
 
 
+
+$(document).ready(function() {
+ $('.gallery-item').magnificPopup({
+  delegate: 'a',		
+  type: 'image',
+  gallery:{
+    enabled:true,
+     preload: [0,2], // read about this option in next Lazy-loading section
+
+  navigateByImgClick: true,
+
+  arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+
+  tPrev: 'Previous (Left arrow key)', // title for left button
+  tNext: 'Next (Right arrow key)', // title for right button
+  tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter
+  }
+});
+});
 
 
--- app/assets/javascripts/categories/my_js.js
+++ app/assets/javascripts/categories/my_js.js
@@ -21,54 +21,69 @@
    }
 
 
+   $(document).ready(function() {
+  $('.column-flex').magnificPopup({
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type:'image',
+    gallery:{
+    enabled:true
+  }
+  });
+ 
+});
+
+
+
+
+
  // ************************************** Modal ****************************************
 
 // Get the image
 
 
-var getImages = document.querySelectorAll(".carrd-img-top");
-for (var modal = 0; modal < getImages.length; modal++) {
-  getImages[modal].id = "myImg" + (modal + 1);
-}
+// var getImages = document.querySelectorAll(".carrd-img-top");
+// for (var modal = 0; modal < getImages.length; modal++) {
+//   getImages[modal].id = "myImg" + (modal + 1);
+// }
 
 
-// get the modal
+// // get the modal
 
 
-var getModal = document.getElementsByClassName("modal");
-console.log(getModal);
+// var getModal = document.getElementsByClassName("modal");
+// console.log(getModal);
 
-for (var m = 0;m<getModal.length;m++){
-  getModal[m].id = "myModal" + (m +1);
-}
+// for (var m = 0;m<getModal.length;m++){
+//   getModal[m].id = "myModal" + (m +1);
+// }
 
-// get the modal content
+// // get the modal content
 
-var getModalContent = document.getElementsByClassName('modal-content');
-for (var d = 0; d<getModalContent.length; d++) {
-  getModalContent[d].id =  "myModalContent" + (d +1);
-}
+// var getModalContent = document.getElementsByClassName('modal-content');
+// for (var d = 0; d<getModalContent.length; d++) {
+//   getModalContent[d].id =  "myModalContent" + (d +1);
+// }
 
-// Get the <span> element that closes the modal
-var mySpan = document.getElementsByClassName("close");
+// // Get the <span> element that closes the modal
+// var mySpan = document.getElementsByClassName("close");
 
 
 
-for (var x = 0; x<getImages.length; x++) {
-	getImages[x].onclick = function() {
-    for (var tg = 0; tg <getModal.length; tg++) {
-      getModal[tg].style.display = "block";
-      getModalContent[tg].src = this.src;
+// for (var x = 0; x<getImages.length; x++) {
+// 	getImages[x].onclick = function() {
+//     for (var tg = 0; tg <getModal.length; tg++) {
+//       getModal[tg].style.display = "block";
+//       getModalContent[tg].src = this.src;
       
-      mySpan[tg].onclick = function() {
-        for (var l = 0;l<getModal.length;l++){
-          getModal[l].style.display = "none";
+//       mySpan[tg].onclick = function() {
+//         for (var l = 0;l<getModal.length;l++){
+//           getModal[l].style.display = "none";
           
-        }
-      }
-    }
-  }
-}
+//         }
+//       }
+//     }
+//   }
+// }
 
 
 
