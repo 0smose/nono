7fbf65b545e56621bae4ccbc94e106dbd67fd2a1
Robin Dercole <robin.dercole@gmail.com>
2020-02-11 13:41:22 +0100

    removed useless script for automatic scrolling

--- app/assets/javascripts/admins/catalogues/my_js.js
+++
@@ -1,7 +0,0 @@
-   var scroll = document.getElementsByClassName("my-scroll");
-    for (var o = 0; o < scroll.length; o++) {
-      (scroll[o]).onclick = function() {
-        
-        window.scrollTo(0, 2000);
-      }
-    }
